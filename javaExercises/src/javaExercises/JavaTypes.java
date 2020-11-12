1b4c66247eeef6fb0110d08bb55d4af5957e7aeb
James Abney <jameshabney@icloud.com>
2020-11-12 13:28:15 -0500

    adding java exercises

---
+++ javaExercises/src/javaExercises/JavaTypes.java
@@ -0,0 +1,29 @@
+package javaExercises;
+
+
+import java.util.Scanner;
+
+
+
+
+public class JavaTypes {
+
+	public class Car {
+		String brand;
+		//int NumofDoors ="5 doors";
+		
+		public Car();
+		
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		Car myCar = new Car();
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
